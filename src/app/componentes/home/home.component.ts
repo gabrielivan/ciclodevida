53e9781b4460e3a604ad9b392f8d8e7bbcfa36a8
Gabriel <gabrielivansaliba@gmail.com>
2020-04-09 20:43:30 -0300

    update ngIf-ngFor

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  expression = true;
+  lista = [{name:"Matias", perfil: 1}, {name:"Octavio", perfil: 2}, {name:"Gabriel", perfil: 3}]
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+   OcultarMostrar() {
+    this.expression = !this.expression;
+  }
 }
